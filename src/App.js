dab2e731aa2d098dd87efde5260b4bbb209f118f
wonderLuc <workmaildo@yandex.ru>
2021-03-22 00:18:47 +0500

    feat: add header and footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import Main from "./Main/Main.js";
 
 function App() {
   return (
-    <Main reports={[{text:"Hello"},{text:"World"}]}/>
+    <div>
+      <header>
+        <h2>Отчеты</h2>
+      </header>
+      <Main reports={[{text:"Hello"},{text:"World"}]}/>
+      <footer>
+        <h2><a hfer="">Обратная связь</a></h2>
+      </footer>
+    </div>
   );
 }
 
