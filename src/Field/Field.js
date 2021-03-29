80d91cf1300a0f13375d67413c25ec40bbde684e
wonderLuc <workmaildo@yandex.ru>
2021-03-29 10:43:24 +0500

    refactor: add dictionary for property names

--- src/Field/Field.js
+++ src/Field/Field.js
@@ -1,16 +1,27 @@
 import "./Field.css";
 
 function Field(props){
-  let className = "field__input " + props.options.class
+  let className = "field__input " + 
     props.options.class
     .split(" ")
     .reduce((acc, current)=>{
       return acc + `field__input_${current} `;
     }, '');
 
+    let dictionary ={
+      'Всего': 'All',
+      'Неотложных': 'urgent',
+      'Диспансер. набор': 'dispensary',
+      'Разовые': 'once',
+      'Закрытые обращения': 'closed',
+      'СТВ': 'ovw',
+      'Профосмотры': 'inspection',
+      'Прочие': 'other',
+      'Посещения СТВ': 'ovwVisits'
+    }
     let changeHandler = (e)=>{
       let value = e.target.value;
-      let name = props.options.text;
+      let name = dictionary[props.options.text] || props.options.text;
       let newState = {};
       newState[''+name] = value;
       props.changeHandler(prevState=>{
