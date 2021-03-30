09c4e8be277754bf9bfc219405e0350f74de37f8
wonderLuc <workmaildo@yandex.ru>
2021-03-30 23:27:44 +0500

    feat: add basic server with two parts: app and api

---
+++ server.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let api = require("./server/paths/api.js");
+
+const PORT = process.env.PORT || 80;
+
+
+
+const app = express();
+app.use(express.json());
+
+// App part
+app.get('/',(req,res)=>{
+  res.set({
+    'Access-Control-Allow-Origin':'*',
+    'Access-Control-Allow-Methods':'POST, GET, OPTIONS',
+  })
+  res.send('<h1>Reports</h1>');
+});
+
+
+
+// API part
+app.use('/api', api);
+
+
+//404 page
+app.get('*',(req,res)=>{
+  res.status(404);
+  res.send('<h1>404</h1>');
+})
+app.listen(PORT,()=>{
+  console.log(`Listening at ${PORT}`);
+});
+
